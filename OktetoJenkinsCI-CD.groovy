7f24a418b534c8dd9ffaae15b6aabffd54bafb5c
Carlos Alvarado Martínez <carlos-alvarado@outlook.com>
2020-10-17 00:00:05 -0400

    Groovy Pipeline

--- OktetoJenkinsCI-CD.groovy
+++ OktetoJenkinsCI-CD.groovy
@@ -2,8 +2,8 @@
 
 //Author: Carlos Alvarado
 //Jenkins Pipeline to handle the Continuous Integration and Continuous Deployment on Okteto.
-//As prerequisites, you should install the Custom tools plugin on Jenkins... 
-//...get the okteto CLI and Kubectl and you need to get your Okteto Token as well, and save it on a Jenkins Credential
+//Prerequisites: you should install the Custom tools plugin on Jenkins, ... 
+//...get the okteto CLI and Kubectl. You also need to get your Okteto Token and save it on a Jenkins Credential
 
 
 node {
@@ -19,7 +19,6 @@ node {
             cleanWs deleteDirs: true
             def output = sh returnStdout: true, script: '''
             ${OKTETO_DIR}/okteto login --token ${SECRET}
-            ${OKTETO_DIR}/okteto namespace
             '''
             println output
         }
@@ -32,7 +31,6 @@ node {
     stage ('Build and Push Image with Okteto'){
         withCredentials([string(credentialsId: 'okteto-token', variable: 'SECRET')]) {
             def output = sh returnStdout: true, script: '''${OKTETO_DIR}/okteto login --token ${SECRET}
-            ${OKTETO_DIR}/okteto namespace
             cd ${HOME}/personas_django
             ${OKTETO_DIR}/okteto build -t ${CONTAINER_IMAGE}:${BUILD_TAG} .
             '''
